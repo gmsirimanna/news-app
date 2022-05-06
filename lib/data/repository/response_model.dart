87116351c71e24023add93ff0774b4a7608bcb42
Gayan Sirimanna <gmsirimanna@gmail.com>
2022-05-06 22:00:50 +0530

    Revert "UI and API implement"
    This reverts commit 4f9368881be1487be09b4df185e7522a98e1d5e4.

--- lib/data/repository/response_model.dart
+++
@@ -1,9 +0,0 @@
-class ResponseModel {
-  String _message;
-  bool _isSuccess;
-
-  ResponseModel(this._isSuccess, this._message);
-
-  bool get isSuccess => _isSuccess;
-  String get message => _message;
-}
